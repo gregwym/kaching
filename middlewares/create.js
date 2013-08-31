d4d1b862eeedb00edcae7fe9536bbe7f4f14fe07
Greg Wang <gregwym@gmail.com>
2013-08-31 20:25:09 +0800

    Refactor create middleware use initStrategy.

--- middlewares/create.js
+++ middlewares/create.js
@@ -1,5 +1,4 @@
-var Context = require('../context/context'),
-    actions = require('../context/actions');
+var initStrategy = require('./initStrategy');
 
 /**
  * Create a payment.
@@ -21,18 +20,7 @@ module.exports = function(name, options, callback) {
   return function create(req, res, next) {
     // Find the specified strategy
     var kaching = this;
-    var prototype = kaching._strategies[name];
-    if (!prototype) { throw new Error('no strategy registered under name: ' + name); }
-
-    // Create a new strategy object base on the given one.
-    // Bind actions with context to the strategy.
-    var strategy = Object.create(prototype);
-    var context = new Context(null, req, res, next);
-    augment(strategy, actions, context);
-
-    // Prepare strategy session namespace
-    req._kaching.session[name] = req._kaching.session[name] || {};
-    strategy.session = req._kaching.session[name];
+    var strategy = initStrategy(kaching, name, res, req, next);
 
     // Execute create
     strategy.create(req.payment || req.body, options, function(err, payment) {
